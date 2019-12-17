e15b19e514a28ef21fc2ff70c554b6f0685b2c77
Aaron-K-T-Berry <aaron.abm@gmail.com>
2019-12-17 12:49:34 +1100

    reorganised styles

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ var Handlebars = require("handlebars");
 var moment = require("moment");
 
 function render(resume) {
-	var css = fs.readFileSync(__dirname + "/style.css", "utf-8");
+	var main_css = fs.readFileSync(__dirname + "/styles/main.css", "utf-8");
+	var comp_css = fs.readFileSync(__dirname + "/styles/components.css", "utf-8");
 	var tpl = fs.readFileSync(__dirname + "/resume.hbs", "utf-8");
-	
+
 	return Handlebars.compile(tpl)({
-		css: css,
+		css: main_css + comp_css,
 		resume: resume
 	});
 }
@@ -16,40 +17,39 @@ module.exports = {
 	render: render
 };
 
-
 /* HANDLEBARS HELPERS */
-Handlebars.registerHelper('paragraphSplit', function(plaintext) {
-    var lines = plaintext.split(/\r\n|\r|\n/g);
-    var output = '';
-    var i;
+Handlebars.registerHelper("paragraphSplit", function(plaintext) {
+	var lines = plaintext.split(/\r\n|\r|\n/g);
+	var output = "";
+	var i;
 
-    for (i = 0; i < lines.length; i+=1) {
-        if(lines[i]) {
-            output += '<p>' + lines[i] + '</p>';
-        }
-    }
-    return new Handlebars.SafeString(output);
+	for (i = 0; i < lines.length; i += 1) {
+		if (lines[i]) {
+			output += "<p>" + lines[i] + "</p>";
+		}
+	}
+	return new Handlebars.SafeString(output);
 });
 
-Handlebars.registerHelper('toLowerCase', function(str) {
-  return str.toLowerCase();
+Handlebars.registerHelper("toLowerCase", function(str) {
+	return str.toLowerCase();
 });
 
-Handlebars.registerHelper('spaceToDash', function(str) {
-  return str.replace(/\s/g, '-').toLowerCase();
+Handlebars.registerHelper("spaceToDash", function(str) {
+	return str.replace(/\s/g, "-").toLowerCase();
 });
 
-Handlebars.registerHelper('MY', function(date) {
+Handlebars.registerHelper("MY", function(date) {
 	var d = date.toString();
-	return moment(d).format('MMMM YYYY');
+	return moment(d).format("MMMM YYYY");
 });
 
-Handlebars.registerHelper('Y', function(date) {
+Handlebars.registerHelper("Y", function(date) {
 	var d = date.toString();
-  return moment(d).format('YYYY');
+	return moment(d).format("YYYY");
 });
 
-Handlebars.registerHelper('DMY', function(date) {
+Handlebars.registerHelper("DMY", function(date) {
 	var d = date.toString();
-  return moment(d).format('D MMMM YYYY');
+	return moment(d).format("D MMMM YYYY");
 });
